1c59a716c71f48a07be9626ff179257d89befd29
Gang Deng <13918383742@163.com>
2021-11-24 08:57:06 +0800

    add BOM and format

--- 点云库PCL从入门到精通/第六章/1 passthrough/source/passthrough.cpp
+++ 点云库PCL从入门到精通/第六章/1 passthrough/source/passthrough.cpp
@@ -1,39 +1,37 @@
-#include <iostream>
-#include <ctime>
+ï»¿#include <pcl/filters/passthrough.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/passthrough.h>
-int
- main (int argc, char** argv)
-{ srand(time(0));
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-  //ÌîÈëµãÔÆÊý¾Ý
-  cloud->width  = 5;
+#include <ctime>
+#include <iostream>
+int main(int argc, char** argv) {
+  srand(time(0));
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(
+      new pcl::PointCloud<pcl::PointXYZ>);
+  //å¡«å…¥ç‚¹äº‘æ•°æ®
+  cloud->width = 5;
   cloud->height = 1;
-  cloud->points.resize (cloud->width * cloud->height);
-  for (size_t i = 0; i < cloud->points.size (); ++i)
-  {
-    cloud->points[i].x = rand () / (RAND_MAX + 1.0f)-0.5;
-    cloud->points[i].y = rand () / (RAND_MAX + 1.0f)-0.5;
-    cloud->points[i].z = rand () / (RAND_MAX + 1.0f)-0.5;
+  cloud->points.resize(cloud->width * cloud->height);
+  for (size_t i = 0; i < cloud->points.size(); ++i) {
+    cloud->points[i].x = rand() / (RAND_MAX + 1.0f) - 0.5;
+    cloud->points[i].y = rand() / (RAND_MAX + 1.0f) - 0.5;
+    cloud->points[i].z = rand() / (RAND_MAX + 1.0f) - 0.5;
   }
   std::cerr << "Cloud before filtering: " << std::endl;
-  for (size_t i = 0; i < cloud->points.size (); ++i)
-    std::cerr << "    " << cloud->points[i].x << " " 
-                        << cloud->points[i].y << " " 
-                        << cloud->points[i].z << std::endl;
-  // ´´½¨ÂË²¨Æ÷¶ÔÏó
+  for (size_t i = 0; i < cloud->points.size(); ++i)
+    std::cerr << "    " << cloud->points[i].x << " " << cloud->points[i].y
+              << " " << cloud->points[i].z << std::endl;
+  // åˆ›å»ºæ»¤æ³¢å™¨å¯¹è±¡
   pcl::PassThrough<pcl::PointXYZ> pass;
-  pass.setInputCloud (cloud);
-  pass.setFilterFieldName ("z");
-  pass.setFilterLimits (0.0, 1.0);
-  //pass.setFilterLimitsNegative (true);
-  pass.filter (*cloud_filtered);
+  pass.setInputCloud(cloud);
+  pass.setFilterFieldName("z");
+  pass.setFilterLimits(0.0, 1.0);
+  // pass.setFilterLimitsNegative (true);
+  pass.filter(*cloud_filtered);
 
   std::cerr << "Cloud after filtering: " << std::endl;
-  for (size_t i = 0; i < cloud_filtered->points.size (); ++i)
-    std::cerr << "    " << cloud_filtered->points[i].x << " " 
-                        << cloud_filtered->points[i].y << " " 
-                        << cloud_filtered->points[i].z << std::endl;
+  for (size_t i = 0; i < cloud_filtered->points.size(); ++i)
+    std::cerr << "    " << cloud_filtered->points[i].x << " "
+              << cloud_filtered->points[i].y << " "
+              << cloud_filtered->points[i].z << std::endl;
   return (0);
 }
--- 点云库PCL从入门到精通/第六章/3 statistical_removal/source/statistical_removal.cpp
+++ 点云库PCL从入门到精通/第六章/3 statistical_removal/source/statistical_removal.cpp
@@ -1,30 +1,31 @@
-#include <iostream>
+ï»¿#include <pcl/filters/statistical_outlier_removal.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <pcl/filters/statistical_outlier_removal.h>
-int
-main (int argc, char** argv)
-{
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered (new pcl::PointCloud<pcl::PointXYZ>);
-  // ÌîÈëµãÔÆÊı¾İ
+#include <iostream>
+int main(int argc, char** argv) {
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_filtered(
+      new pcl::PointCloud<pcl::PointXYZ>);
+  // å¡«å…¥ç‚¹äº‘æ•°æ®
   pcl::PCDReader reader;
-  // °ÑÂ·¾¶¸ÄÎª×Ô¼º´æ·ÅÎÄ¼şµÄÂ·¾¶
-  reader.read<pcl::PointXYZ> ("table_scene_lms400.pcd", *cloud);
+  // æŠŠè·¯å¾„æ”¹ä¸ºè‡ªå·±å­˜æ”¾æ–‡ä»¶çš„è·¯å¾„
+  reader.read<pcl::PointXYZ>("table_scene_lms400.pcd", *cloud);
   std::cerr << "Cloud before filtering: " << std::endl;
   std::cerr << *cloud << std::endl;
-  // ´´½¨ÂË²¨Æ÷¶ÔÏó
+  // åˆ›å»ºæ»¤æ³¢å™¨å¯¹è±¡
   pcl::StatisticalOutlierRemoval<pcl::PointXYZ> sor;
-  sor.setInputCloud (cloud);
-  sor.setMeanK (50);
-  sor.setStddevMulThresh (1.0);
-  sor.filter (*cloud_filtered);
+  sor.setInputCloud(cloud);
+  sor.setMeanK(50);
+  sor.setStddevMulThresh(1.0);
+  sor.filter(*cloud_filtered);
   std::cerr << "Cloud after filtering: " << std::endl;
   std::cerr << *cloud_filtered << std::endl;
   pcl::PCDWriter writer;
-  writer.write<pcl::PointXYZ> ("table_scene_lms400_inliers.pcd", *cloud_filtered, false);
-  sor.setNegative (true);
-  sor.filter (*cloud_filtered);
-  writer.write<pcl::PointXYZ> ("table_scene_lms400_outliers.pcd", *cloud_filtered, false);
+  writer.write<pcl::PointXYZ>("table_scene_lms400_inliers.pcd", *cloud_filtered,
+                              false);
+  sor.setNegative(true);
+  sor.filter(*cloud_filtered);
+  writer.write<pcl::PointXYZ>("table_scene_lms400_outliers.pcd",
+                              *cloud_filtered, false);
   return (0);
 }
