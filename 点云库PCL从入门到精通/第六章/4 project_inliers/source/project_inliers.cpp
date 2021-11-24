cba9c0599dac4706ba61edbda512411bcd82d56b
Gang Deng <13918383742@163.com>
2021-11-24 09:10:21 +0800

    add BOM and format

--- 点云库PCL从入门到精通/第六章/4 project_inliers/source/project_inliers.cpp
+++ 点云库PCL从入门到精通/第六章/4 project_inliers/source/project_inliers.cpp
@@ -1,46 +1,43 @@
-#include <iostream>
+ï»¿#include <pcl/ModelCoefficients.h>
+#include <pcl/filters/project_inliers.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
-#include <pcl/ModelCoefficients.h>
-#include <pcl/filters/project_inliers.h>
-int
- main (int argc, char** argv)
-{
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected (new pcl::PointCloud<pcl::PointXYZ>);
-  // ÌîÈëµãÔÆÊý¾Ý
-  cloud->width  = 5;
+#include <iostream>
+int main(int argc, char** argv) {
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_projected(
+      new pcl::PointCloud<pcl::PointXYZ>);
+  // å¡«å…¥ç‚¹äº‘æ•°æ®
+  cloud->width = 5;
   cloud->height = 1;
-  cloud->points.resize (cloud->width * cloud->height);
-  for (size_t i = 0; i < cloud->points.size (); ++i)
-  {
-    cloud->points[i].x = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud->points[i].y = 1024 * rand () / (RAND_MAX + 1.0f);
-    cloud->points[i].z = 1024 * rand () / (RAND_MAX + 1.0f);
+  cloud->points.resize(cloud->width * cloud->height);
+  for (size_t i = 0; i < cloud->points.size(); ++i) {
+    cloud->points[i].x = 1024 * rand() / (RAND_MAX + 1.0f);
+    cloud->points[i].y = 1024 * rand() / (RAND_MAX + 1.0f);
+    cloud->points[i].z = 1024 * rand() / (RAND_MAX + 1.0f);
   }
   std::cerr << "Cloud before projection: " << std::endl;
-  for (size_t i = 0; i < cloud->points.size (); ++i)
-    std::cerr << "    " << cloud->points[i].x << " " 
-                        << cloud->points[i].y << " " 
-                        << cloud->points[i].z << std::endl;
-  // ´´½¨Ò»¸öÏµÊýÎªX=Y=0,Z=1µÄÆ½Ãæ
-  pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients ());
-  coefficients->values.resize (4);
+  for (size_t i = 0; i < cloud->points.size(); ++i)
+    std::cerr << "    " << cloud->points[i].x << " " << cloud->points[i].y
+              << " " << cloud->points[i].z << std::endl;
+  // åˆ›å»ºä¸€ä¸ªç³»æ•°ä¸ºX=Y=0,Z=1çš„å¹³é¢
+  pcl::ModelCoefficients::Ptr coefficients(new pcl::ModelCoefficients());
+  coefficients->values.resize(4);
   coefficients->values[0] = coefficients->values[1] = 0;
   coefficients->values[2] = 1.0;
   coefficients->values[3] = 0;
-  // ´´½¨ÂË²¨Æ÷¶ÔÏó
+  // åˆ›å»ºæ»¤æ³¢å™¨å¯¹è±¡
   pcl::ProjectInliers<pcl::PointXYZ> proj;
-  proj.setModelType (pcl::SACMODEL_PLANE);
-  proj.setInputCloud (cloud);
-  proj.setModelCoefficients (coefficients);
-  proj.filter (*cloud_projected);
+  proj.setModelType(pcl::SACMODEL_PLANE);
+  proj.setInputCloud(cloud);
+  proj.setModelCoefficients(coefficients);
+  proj.filter(*cloud_projected);
 
   std::cerr << "Cloud after projection: " << std::endl;
-  for (size_t i = 0; i < cloud_projected->points.size (); ++i)
-    std::cerr << "    " << cloud_projected->points[i].x << " " 
-                        << cloud_projected->points[i].y << " " 
-                        << cloud_projected->points[i].z << std::endl;
+  for (size_t i = 0; i < cloud_projected->points.size(); ++i)
+    std::cerr << "    " << cloud_projected->points[i].x << " "
+              << cloud_projected->points[i].y << " "
+              << cloud_projected->points[i].z << std::endl;
 
   return (0);
 }
