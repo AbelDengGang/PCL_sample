ab66310c7f09f397e50c35d02f7cc26d6d461c2d
Gang Deng <13918383742@163.com>
2021-11-25 09:46:18 +0800

    fix build issue, add BOM, format

--- 点云库PCL从入门到精通/第十章/3 narf_feature_extraction/source/narf_feature_extraction.cpp
+++ 点云库PCL从入门到精通/第十章/3 narf_feature_extraction/source/narf_feature_extraction.cpp
@@ -1,48 +1,54 @@
-/* \author Bastian Steder */
-#include <iostream>
-#include <boost/thread/thread.hpp>
-#include <pcl/range_image/range_image.h>
-#include <pcl/io/pcd_io.h>
-#include <pcl/visualization/range_image_visualizer.h>
-#include <pcl/visualization/pcl_visualizer.h>
-#include <pcl/features/range_image_border_extractor.h>
-#include <pcl/keypoints/narf_keypoint.h>
-#include <pcl/features/narf_descriptor.h>
+Ôªø/* \author Bastian Steder */
 #include <pcl/console/parse.h>
+#include <pcl/features/narf_descriptor.h>
+#include <pcl/features/range_image_border_extractor.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/keypoints/narf_keypoint.h>
+#include <pcl/range_image/range_image.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/visualization/range_image_visualizer.h>
+#include <boost/thread/thread.hpp>
+#include <iostream>
 
 typedef pcl::PointXYZ PointType;
-// -----≤Œ ˝-----
+// -----ÂèÇÊï∞-----
 // --------------------
 float angular_resolution = 0.5f;
 float support_size = 0.2f;
-pcl::RangeImage::CoordinateFrame coordinate_frame = pcl::RangeImage::CAMERA_FRAME;
+pcl::RangeImage::CoordinateFrame coordinate_frame =
+    pcl::RangeImage::CAMERA_FRAME;
 bool setUnseenToMaxRange = false;
 bool rotation_invariant = true;
 
-// -----∞Ô÷˙-----
-void 
-printUsage (const char* progName)
-{
-  std::cout << "\n\nUsage: "<<progName<<" [options] <scene.pcd>\n\n"
+// -----Â∏ÆÂä©-----
+void printUsage(const char* progName) {
+  std::cout << "\n\nUsage: " << progName << " [options] <scene.pcd>\n\n"
             << "Options:\n"
             << "-------------------------------------------\n"
-            << "-r <float>   angular resolution in degrees (default "<<angular_resolution<<")\n"
-            << "-c <int>     coordinate frame (default "<< (int)coordinate_frame<<")\n"
+            << "-r <float>   angular resolution in degrees (default "
+            << angular_resolution << ")\n"
+            << "-c <int>     coordinate frame (default "
+            << (int)coordinate_frame << ")\n"
             << "-m           Treat all unseen points to max range\n"
-            << "-s <float>   support size for the interest points (diameter of the used sphere - "
-                                                                  "default "<<support_size<<")\n"
-            << "-o <0/1>     switch rotational invariant version of the feature on/off"
-            <<               " (default "<< (int)rotation_invariant<<")\n"
+            << "-s <float>   support size for the interest points (diameter of "
+               "the used sphere - "
+               "default "
+            << support_size << ")\n"
+            << "-o <0/1>     switch rotational invariant version of the "
+               "feature on/off"
+            << " (default " << (int)rotation_invariant << ")\n"
             << "-h           this help\n"
             << "\n\n";
 }
 
-void 
-setViewerPose (pcl::visualization::PCLVisualizer& viewer, const Eigen::Affine3f& viewer_pose)
-{
-  Eigen::Vector3f pos_vector = viewer_pose * Eigen::Vector3f (0, 0, 0);
-  Eigen::Vector3f look_at_vector = viewer_pose.rotation () * Eigen::Vector3f (0, 0, 1) + pos_vector;
-  Eigen::Vector3f up_vector = viewer_pose.rotation () * Eigen::Vector3f (0, -1, 0);
+void setViewerPose(pcl::visualization::PCLVisualizer& viewer,
+                   const Eigen::Affine3f& viewer_pose) {
+  Eigen::Vector3f pos_vector = viewer_pose * Eigen::Vector3f(0, 0, 0);
+  Eigen::Vector3f look_at_vector =
+      viewer_pose.rotation() * Eigen::Vector3f(0, 0, 1) + pos_vector;
+  Eigen::Vector3f up_vector =
+      viewer_pose.rotation() * Eigen::Vector3f(0, -1, 0);
+#if 0
   viewer.camera_.pos[0] = pos_vector[0];
   viewer.camera_.pos[1] = pos_vector[1];
   viewer.camera_.pos[2] = pos_vector[2];
@@ -52,137 +58,152 @@ setViewerPose (pcl::visualization::PCLVisualizer& viewer, const Eigen::Affine3f&
   viewer.camera_.view[0] = up_vector[0];
   viewer.camera_.view[1] = up_vector[1];
   viewer.camera_.view[2] = up_vector[2];
-  viewer.updateCamera ();
+#endif
+  viewer.setCameraPosition(pos_vector[0], pos_vector[1], pos_vector[2],
+                           look_at_vector[0], look_at_vector[1],
+                           look_at_vector[2], up_vector[0], up_vector[1],
+                           up_vector[2]);
+  viewer.updateCamera();
 }
-// -----÷˜∫Ø ˝-----
+// -----‰∏ªÂáΩÊï∞-----
 // --------------
-int 
-main (int argc, char** argv)
-{
-  // -----Ω‚Œˆ√¸¡Ó––≤Œ ˝-----
-  if (pcl::console::find_argument (argc, argv, "-h") >= 0)
-  {
-    printUsage (argv[0]);
+int main(int argc, char** argv) {
+  // -----Ëß£ÊûêÂëΩ‰ª§Ë°åÂèÇÊï∞-----
+  if (pcl::console::find_argument(argc, argv, "-h") >= 0) {
+    printUsage(argv[0]);
     return 0;
   }
-  if (pcl::console::find_argument (argc, argv, "-m") >= 0)
-  {
+  if (pcl::console::find_argument(argc, argv, "-m") >= 0) {
     setUnseenToMaxRange = true;
     cout << "Setting unseen values in range image to maximum range readings.\n";
   }
-  if (pcl::console::parse (argc, argv, "-o", rotation_invariant) >= 0)
-    cout << "Switching rotation invariant feature version "<< (rotation_invariant ? "on" : "off")<<".\n";
+  if (pcl::console::parse(argc, argv, "-o", rotation_invariant) >= 0)
+    cout << "Switching rotation invariant feature version "
+         << (rotation_invariant ? "on" : "off") << ".\n";
   int tmp_coordinate_frame;
-  if (pcl::console::parse (argc, argv, "-c", tmp_coordinate_frame) >= 0)
-  {
-    coordinate_frame = pcl::RangeImage::CoordinateFrame (tmp_coordinate_frame);
-    cout << "Using coordinate frame "<< (int)coordinate_frame<<".\n";
+  if (pcl::console::parse(argc, argv, "-c", tmp_coordinate_frame) >= 0) {
+    coordinate_frame = pcl::RangeImage::CoordinateFrame(tmp_coordinate_frame);
+    cout << "Using coordinate frame " << (int)coordinate_frame << ".\n";
   }
-  if (pcl::console::parse (argc, argv, "-s", support_size) >= 0)
-    cout << "Setting support size to "<<support_size<<".\n";
-  if (pcl::console::parse (argc, argv, "-r", angular_resolution) >= 0)
-    cout << "Setting angular resolution to "<<angular_resolution<<"deg.\n";
-  angular_resolution = pcl::deg2rad (angular_resolution);
-// -----∂¡»°pcdŒƒº˛£¨ªÚ’ﬂ»Áπ˚√ª”–∏¯≥ˆ£¨‘Ú¥¥Ω®“ª∏ˆ—˘±æµ„‘∆-----
-  pcl::PointCloud<PointType>::Ptr point_cloud_ptr (new pcl::PointCloud<PointType>);
+  if (pcl::console::parse(argc, argv, "-s", support_size) >= 0)
+    cout << "Setting support size to " << support_size << ".\n";
+  if (pcl::console::parse(argc, argv, "-r", angular_resolution) >= 0)
+    cout << "Setting angular resolution to " << angular_resolution << "deg.\n";
+  angular_resolution = pcl::deg2rad(angular_resolution);
+  // -----ËØªÂèñpcdÊñá‰ª∂ÔºåÊàñËÄÖÂ¶ÇÊûúÊ≤°ÊúâÁªôÂá∫ÔºåÂàôÂàõÂª∫‰∏Ä‰∏™Ê†∑Êú¨ÁÇπ‰∫ë-----
+  pcl::PointCloud<PointType>::Ptr point_cloud_ptr(
+      new pcl::PointCloud<PointType>);
   pcl::PointCloud<PointType>& point_cloud = *point_cloud_ptr;
   pcl::PointCloud<pcl::PointWithViewpoint> far_ranges;
-  Eigen::Affine3f scene_sensor_pose (Eigen::Affine3f::Identity ());
-  std::vector<int> pcd_filename_indices = pcl::console::parse_file_extension_argument (argc, argv, "pcd");
-  if (!pcd_filename_indices.empty ())
-  {
+  Eigen::Affine3f scene_sensor_pose(Eigen::Affine3f::Identity());
+  std::vector<int> pcd_filename_indices =
+      pcl::console::parse_file_extension_argument(argc, argv, "pcd");
+  if (!pcd_filename_indices.empty()) {
     std::string filename = argv[pcd_filename_indices[0]];
-    if (pcl::io::loadPCDFile (filename, point_cloud) == -1)
-    {
-      cerr << "Was not able to open file \""<<filename<<"\".\n";
-      printUsage (argv[0]);
+    if (pcl::io::loadPCDFile(filename, point_cloud) == -1) {
+      cerr << "Was not able to open file \"" << filename << "\".\n";
+      printUsage(argv[0]);
       return 0;
     }
-    scene_sensor_pose = Eigen::Affine3f (Eigen::Translation3f (point_cloud.sensor_origin_[0],                                                               point_cloud.sensor_origin_[1],                                             point_cloud.sensor_origin_[2])) *
-                        Eigen::Affine3f (point_cloud.sensor_orientation_);
-    std::string far_ranges_filename = pcl::getFilenameWithoutExtension (filename)+"_far_ranges.pcd";
-    if (pcl::io::loadPCDFile (far_ranges_filename.c_str (), far_ranges) == -1)
-      std::cout << "Far ranges file \""<<far_ranges_filename<<"\" does not exists.\n";
-  }
-  else
-  {
+    scene_sensor_pose =
+        Eigen::Affine3f(Eigen::Translation3f(point_cloud.sensor_origin_[0],
+                                             point_cloud.sensor_origin_[1],
+                                             point_cloud.sensor_origin_[2])) *
+        Eigen::Affine3f(point_cloud.sensor_orientation_);
+    std::string far_ranges_filename =
+        pcl::getFilenameWithoutExtension(filename) + "_far_ranges.pcd";
+    if (pcl::io::loadPCDFile(far_ranges_filename.c_str(), far_ranges) == -1)
+      std::cout << "Far ranges file \"" << far_ranges_filename
+                << "\" does not exists.\n";
+  } else {
     setUnseenToMaxRange = true;
     cout << "\nNo *.pcd file given => Genarating example point cloud.\n\n";
-    for (float x=-0.5f; x<=0.5f; x+=0.01f)
-    {
-      for (float y=-0.5f; y<=0.5f; y+=0.01f)
-      {
-        PointType point;  point.x = x;  point.y = y;  point.z = 2.0f - y;
-        point_cloud.points.push_back (point);
+    for (float x = -0.5f; x <= 0.5f; x += 0.01f) {
+      for (float y = -0.5f; y <= 0.5f; y += 0.01f) {
+        PointType point;
+        point.x = x;
+        point.y = y;
+        point.z = 2.0f - y;
+        point_cloud.points.push_back(point);
       }
     }
-    point_cloud.width = (int) point_cloud.points.size ();  point_cloud.height = 1;
+    point_cloud.width = (int)point_cloud.points.size();
+    point_cloud.height = 1;
   }
- // -----¥”µ„‘∆÷–¥¥Ω®…Ó∂»ÕºœÒ----
-float noise_level = 0.0;
+  // -----‰ªéÁÇπ‰∫ë‰∏≠ÂàõÂª∫Ê∑±Â∫¶ÂõæÂÉè----
+  float noise_level = 0.0;
   float min_range = 0.0f;
   int border_size = 1;
-  boost::shared_ptr<pcl::RangeImage> range_image_ptr (new pcl::RangeImage);
-  pcl::RangeImage& range_image = *range_image_ptr;   
-  range_image.createFromPointCloud (point_cloud, angular_resolution, pcl::deg2rad (360.0f), pcl::deg2rad (180.0f),
-                                   scene_sensor_pose, coordinate_frame, noise_level, min_range, border_size);
-  range_image.integrateFarRanges (far_ranges);
-  if (setUnseenToMaxRange)
-    range_image.setUnseenToMaxRange ();
-  
-  // -----¥Úø™»˝Œ¨π€≤Ï“«≤¢º”»Îµ„‘∆-----
-  pcl::visualization::PCLVisualizer viewer ("3D Viewer");
-  viewer.setBackgroundColor (1, 1, 1);
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointWithRange> range_image_color_handler (range_image_ptr, 0, 0, 0);
-  viewer.addPointCloud (range_image_ptr, range_image_color_handler, "range image");
-  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "range image");
-  viewer.initCameraParameters ();
-  setViewerPose (viewer, range_image.getTransformationToWorldSystem ());  
-  // -----œ‘ æ…Ó∂»ÕºœÒ-----
-  pcl::visualization::RangeImageVisualizer  range_image_widget ("Range image");
-  range_image_widget.showRangeImage (range_image);
-  // -----Ã·»° NARF πÿº¸µ„-----
+  boost::shared_ptr<pcl::RangeImage> range_image_ptr(new pcl::RangeImage);
+  pcl::RangeImage& range_image = *range_image_ptr;
+  range_image.createFromPointCloud(point_cloud, angular_resolution,
+                                   pcl::deg2rad(360.0f), pcl::deg2rad(180.0f),
+                                   scene_sensor_pose, coordinate_frame,
+                                   noise_level, min_range, border_size);
+  range_image.integrateFarRanges(far_ranges);
+  if (setUnseenToMaxRange) range_image.setUnseenToMaxRange();
+
+  // -----ÊâìÂºÄ‰∏âÁª¥ËßÇÂØü‰ª™Âπ∂Âä†ÂÖ•ÁÇπ‰∫ë-----
+  pcl::visualization::PCLVisualizer viewer("3D Viewer");
+  viewer.setBackgroundColor(1, 1, 1);
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointWithRange>
+      range_image_color_handler(range_image_ptr, 0, 0, 0);
+  viewer.addPointCloud(range_image_ptr, range_image_color_handler,
+                       "range image");
+  viewer.setPointCloudRenderingProperties(
+      pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "range image");
+  viewer.initCameraParameters();
+  setViewerPose(viewer, range_image.getTransformationToWorldSystem());
+  // -----ÊòæÁ§∫Ê∑±Â∫¶ÂõæÂÉè-----
+  pcl::visualization::RangeImageVisualizer range_image_widget("Range image");
+  range_image_widget.showRangeImage(range_image);
+  // -----ÊèêÂèñ NARF ÂÖ≥ÈîÆÁÇπ-----
   pcl::RangeImageBorderExtractor range_image_border_extractor;
   pcl::NarfKeypoint narf_keypoint_detector;
-  narf_keypoint_detector.setRangeImageBorderExtractor (&range_image_border_extractor);
-  narf_keypoint_detector.setRangeImage (&range_image);
-  narf_keypoint_detector.getParameters ().support_size = support_size;
-  
+  narf_keypoint_detector.setRangeImageBorderExtractor(
+      &range_image_border_extractor);
+  narf_keypoint_detector.setRangeImage(&range_image);
+  narf_keypoint_detector.getParameters().support_size = support_size;
+
   pcl::PointCloud<int> keypoint_indices;
-  narf_keypoint_detector.compute (keypoint_indices);
-  std::cout << "Found "<<keypoint_indices.points.size ()<<" key points.\n";
-  // -----‘⁄…Ó∂»ÕºœÒ¥∞ø⁄œ‘ æπÿº¸µ„-----
-  //for (size_t i=0; i<keypoint_indices.points.size (); ++i)
-    //range_image_widget.markPoint (keypoint_indices.points[i]%range_image.width,
-                                  //keypoint_indices.points[i]/range_image.width);
-// -----‘⁄»˝Œ¨ ”Õº÷–œ‘ æπÿº¸µ„ -----
-    pcl::PointCloud<pcl::PointXYZ>::Ptr keypoints_ptr (new pcl::PointCloud<pcl::PointXYZ>);
+  narf_keypoint_detector.compute(keypoint_indices);
+  std::cout << "Found " << keypoint_indices.points.size() << " key points.\n";
+  // -----Âú®Ê∑±Â∫¶ÂõæÂÉèÁ™óÂè£ÊòæÁ§∫ÂÖ≥ÈîÆÁÇπ-----
+  // for (size_t i=0; i<keypoint_indices.points.size (); ++i)
+  // range_image_widget.markPoint (keypoint_indices.points[i]%range_image.width,
+  // keypoint_indices.points[i]/range_image.width);
+  // -----Âú®‰∏âÁª¥ËßÜÂõæ‰∏≠ÊòæÁ§∫ÂÖ≥ÈîÆÁÇπ -----
+  pcl::PointCloud<pcl::PointXYZ>::Ptr keypoints_ptr(
+      new pcl::PointCloud<pcl::PointXYZ>);
   pcl::PointCloud<pcl::PointXYZ>& keypoints = *keypoints_ptr;
-  keypoints.points.resize (keypoint_indices.points.size ());
-  for (size_t i=0; i<keypoint_indices.points.size (); ++i)
-    keypoints.points[i].getVector3fMap () = range_image.points[keypoint_indices.points[i]].getVector3fMap ();
-  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ> keypoints_color_handler (keypoints_ptr, 0, 255, 0);
-  viewer.addPointCloud<pcl::PointXYZ> (keypoints_ptr, keypoints_color_handler, "keypoints");
-  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 7, "keypoints");
-  // -----Œ™Ãÿ’˜µ„Ã·»°NARF√Ë ˆ◊”-----
+  keypoints.points.resize(keypoint_indices.points.size());
+  for (size_t i = 0; i < keypoint_indices.points.size(); ++i)
+    keypoints.points[i].getVector3fMap() =
+        range_image.points[keypoint_indices.points[i]].getVector3fMap();
+  pcl::visualization::PointCloudColorHandlerCustom<pcl::PointXYZ>
+      keypoints_color_handler(keypoints_ptr, 0, 255, 0);
+  viewer.addPointCloud<pcl::PointXYZ>(keypoints_ptr, keypoints_color_handler,
+                                      "keypoints");
+  viewer.setPointCloudRenderingProperties(
+      pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 7, "keypoints");
+  // -----‰∏∫ÁâπÂæÅÁÇπÊèêÂèñNARFÊèèËø∞Â≠ê-----
   std::vector<int> keypoint_indices2;
-  keypoint_indices2.resize (keypoint_indices.points.size ());
-  for (unsigned int i=0; i<keypoint_indices.size (); ++i) 
-//“™µ√µΩ’˝»∑µƒœÚ¡ø¿‡–Õ£¨’‚“ª≤Ω «±ÿ“™µƒ
-    keypoint_indices2[i]=keypoint_indices.points[i];
-  pcl::NarfDescriptor narf_descriptor (&range_image, &keypoint_indices2);
-  narf_descriptor.getParameters ().support_size = support_size;
-  narf_descriptor.getParameters ().rotation_invariant = rotation_invariant;
+  keypoint_indices2.resize(keypoint_indices.points.size());
+  for (unsigned int i = 0; i < keypoint_indices.size(); ++i)
+    //Ë¶ÅÂæóÂà∞Ê≠£Á°ÆÁöÑÂêëÈáèÁ±ªÂûãÔºåËøô‰∏ÄÊ≠•ÊòØÂøÖË¶ÅÁöÑ
+    keypoint_indices2[i] = keypoint_indices.points[i];
+  pcl::NarfDescriptor narf_descriptor(&range_image, &keypoint_indices2);
+  narf_descriptor.getParameters().support_size = support_size;
+  narf_descriptor.getParameters().rotation_invariant = rotation_invariant;
   pcl::PointCloud<pcl::Narf36> narf_descriptors;
-  narf_descriptor.compute (narf_descriptors);
-  cout << "Extracted "<<narf_descriptors.size ()<<" descriptors for "
-                      <<keypoint_indices.points.size ()<< " keypoints.\n";
+  narf_descriptor.compute(narf_descriptors);
+  cout << "Extracted " << narf_descriptors.size() << " descriptors for "
+       << keypoint_indices.points.size() << " keypoints.\n";
   // -----Main loop-----
-  while (!viewer.wasStopped ())
-  {
-range_image_widget.spinOnce ();  
-//¥¶¿Ì”√ªßΩªª• ¬º˛
-    viewer.spinOnce ();
+  while (!viewer.wasStopped()) {
+    range_image_widget.spinOnce();
+    //Â§ÑÁêÜÁî®Êà∑‰∫§‰∫í‰∫ã‰ª∂
+    viewer.spinOnce();
     pcl_sleep(0.01);
   }
 }
